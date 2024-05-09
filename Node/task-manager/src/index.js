eb76c5d0fabb1e17cdccf500423651851962d07d
Abhishek Anand <anand.abhishek1003@gmail.com>
2024-05-09 08:26:16 +0530

    section 14 completed

--- Node/task-manager/src/index.js
+++ Node/task-manager/src/index.js
@@ -15,18 +15,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-//     // const task = await Task.findById('66335f0c57539c33513308c3');
-//     // await task.populate('owner').execPopulate();
-//     // console.log(task.owner);
-
-//     const user = await User.findById('66335e18bb484d30831a52e2');
-//     await user.populate('tasks').execPopulate();
-//     console.log(user.tasks);
-// }
-
-// main();
--- Node/task-manager/src/routers/users.js
+++ Node/task-manager/src/routers/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const router = new express.Router();
+const multer = require('multer');
+const sharp = require('sharp');
+
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+
+const router = new express.Router();
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -92,6 +96,63 @@ router.delete('/users/me', auth, async (req, res) => {
         res.send(req.user);
     } catch (e) {
         res.status(500).send(e);
+    }
+})
+
+const upload = multer({
+    // dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image.'));
+        }
+
+        cb(undefined, true);
+    }
+});
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    try {
+        const buffer = await sharp(req.file.buffer).resize({
+            width: 90,
+            height: 90
+        }).png().toBuffer();
+
+        req.user.avatar = buffer;
+        await req.user.save();
+
+        res.send(`File uploaded successfully.`);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    try {
+        req.user.avatar = undefined;
+        await req.user.save();
+        res.send(`Profile image deleted.`);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(400).send(e);
     }
 })
 
